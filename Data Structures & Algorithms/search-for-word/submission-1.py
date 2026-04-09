e65bc84763a8cdcfd83652a0d3cba32471296f33
buffered <49856439+iampali@users.noreply.github.com>
2026-04-09 18:33:29 +0100

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        row, col = len(board), len(board[0])
+
+        def dfs(r, c, idx):
+            if idx == len(word):
+                return True
+
+            if r < 0 or r >= row or c < 0 or c >= col or board[r][c] != word[idx]:
+                return False
+
+            temp = board[r][c]
+            board[r][c] = "#"  # mark visited
+
+            found = (
+                dfs(r + 1, c, idx + 1) or
+                dfs(r - 1, c, idx + 1) or
+                dfs(r, c + 1, idx + 1) or
+                dfs(r, c - 1, idx + 1)
+            )
+
+            board[r][c] = temp  # backtrack
+            return found
+
+        for i in range(row):
+            for j in range(col):
+                if dfs(i, j, 0):
+                    return True
+
+        return False
